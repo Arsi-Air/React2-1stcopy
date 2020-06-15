7f5f5e8f55d1dd54cd65840976c7801bdf6e0b2b
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-06-15 04:37:28 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,21 @@ function Hi(props) {
   return <div>Hello {props.name}!</div>;
 }
 
-ReactDOM.render(<Hi/>, document.querySelector('#root'));
+ReactDOM.render(<Hi name="Arsi" />, document.querySelector('#root'));
+
+const MediaCard = function({ title, body, imageUrl }) {
+  return <div>
+    <h2>{title}</h2>
+    <p>{body}</p>
+    <img src={imageUrl} alt = "A pic"/>
+  </div>
+}
+
+ReactDOM.render(<MediaCard title= "Cat" body={<strong>This is awesome!</strong>}  imageUrl="https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg" />, document.querySelector('#root'));
+
+const Gate = function({ isOpen }) {
+  return <div>
+    {isOpen === true ? "open" : "closed"};
+  </div>
+}
+
